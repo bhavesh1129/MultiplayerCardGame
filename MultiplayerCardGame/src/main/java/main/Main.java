3388416eaf57559a2c7cc5a94d1e60bca55634fd
Bhavesh Garg <developerbhaveshgarg@gmail.com>
2023-04-11 23:01:13 +0530

    Add static members and static blocks to the Main class

--- MultiplayerCardGame/src/main/java/main/Main.java
+++ MultiplayerCardGame/src/main/java/main/Main.java
@@ -1,5 +1,37 @@
 package main;
 
+import java.util.Stack;
+import entity.Card;
+
 public class Main {
-	
+    // Declaring the deck of cards as a static and final member
+    public static final Stack<Card> deck = new Stack<>();
+    
+    // Declaring the static block to get the deck complete with all different cards
+    // as soon as the class gets loaded to the memory
+    static {
+        // Completing all cards under Diamonds suit
+        for (int i = 1; i <= 13; i++) {
+            Card card = new Card("Diamonds", i);
+            deck.push(card);
+        }
+
+        // Completing all cards under Spades suit
+        for (int i = 1; i <= 13; i++) {
+            Card card = new Card("Spades", i);
+            deck.push(card);
+        }
+
+        // Completing all cards under Hearts suit
+        for (int i = 1; i <= 13; i++) {
+            Card card = new Card("Hearts", i);
+            deck.push(card);
+        }
+
+        // Completing all cards under Clubs suit
+        for (int i = 1; i <= 13; i++) {
+            Card card = new Card("Clubs", i);
+            deck.push(card);
+        }
+    }
 }
