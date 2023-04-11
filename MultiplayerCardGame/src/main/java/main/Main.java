c8b1baf20084a34206b5a232103b3822a6c19a29
Bhavesh Garg <developerbhaveshgarg@gmail.com>
2023-04-11 23:20:00 +0530

    Implemented functionality to create draw pile and discard pile from the remaining cards after distributing to players

--- MultiplayerCardGame/src/main/java/main/Main.java
+++ MultiplayerCardGame/src/main/java/main/Main.java
@@ -1,11 +1,24 @@
 package main;
 
 import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
 import java.util.Stack;
+import java.util.stream.Collectors;
 
 import entity.Card;
 
+/**
+ * 
+ * The Main class contains the main method which starts the game and handles
+ * the input of the number of players, card distribution, draw pile and discard
+ * pile.
+ * 
+ * It also contains a static and final member 'deck' which is a stack of cards,
+ * and a static block which populates the deck with all the cards of different
+ * suits and ranks.
+ *
+ */
 public class Main {
     // Declaring the deck of cards as a static and final member
     public static final Stack<Card> deck = new Stack<>();
@@ -18,19 +31,16 @@ public class Main {
             Card card = new Card("Diamonds", i);
             deck.push(card);
         }
-
         // Completing all cards under Spades suit
         for (int i = 1; i <= 13; i++) {
             Card card = new Card("Spades", i);
             deck.push(card);
         }
-
         // Completing all cards under Hearts suit
         for (int i = 1; i <= 13; i++) {
             Card card = new Card("Hearts", i);
             deck.push(card);
         }
-
         // Completing all cards under Clubs suit
         for (int i = 1; i <= 13; i++) {
             Card card = new Card("Clubs", i);
@@ -38,6 +48,13 @@ public class Main {
         }
     }
 
+    /**
+     * 
+     * The main method starts the game by taking the number of players as input,
+     * distributing cards among all the players, creating draw pile and discard pile
+     * and shuffling the deck of cards before starting the game.
+     *
+     */
     public static void main(String[] args) {
         // Shuffling the deck of cards before starting the game
         Collections.shuffle(deck);
@@ -63,9 +80,25 @@ public class Main {
                 }
                 allPlayerHands.push(singlePlayerCards);
             }
-        } else {
-            System.out.println(
-                    "The number of players you entered is invalid. Please enter a number between 2 and 4 (inclusive).");
         }
+        // Create the draw pile for the game
+        Stack<Card> drawPile = new Stack<>();
+
+        // Add all the undistributed cards to the draw pile
+        drawPile.addAll(deck);
+
+        // Filter the draw pile to get only the number cards except the action cards
+        List<Card> onlyNumberCards = drawPile.stream().filter(
+                card -> (card.getRank() != 1 &&
+                        card.getRank() != 11 &&
+                        card.getRank() != 12 &&
+                        card.getRank() != 13))
+                .collect(Collectors.toList());
+
+        // Create a discard pile
+        Stack<Card> discardPile = new Stack<>();
+
+        // Add a number card from the draw pile to the discard pile to start the game
+        discardPile.push(onlyNumberCards.get(onlyNumberCards.size() - 1));
     }
 }
