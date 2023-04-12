5dd9a3bf99940fb829b2b4c6a34f28efe8815974
Bhavesh Garg <developerbhaveshgarg@gmail.com>
2023-04-12 10:42:44 +0530

    Added class structure for GamePlay with necessary variables and methods

---
+++ MultiplayerCardGame/src/main/java/org/example/entity/GamePlay.java
@@ -0,0 +1,23 @@
+package org.example.entity;
+
+import java.util.ArrayList;
+
+public class GamePlay {
+
+    // ArrayList containing the deck of cards
+    private ArrayList<Card> deck;
+
+    // ArrayList containing all the players in the game
+    private ArrayList<Player> players;
+
+    // ArrayList representing the draw pile of the game
+    private ArrayList<Card> drawPile;
+
+    // ArrayList representing the discard pile of the game
+    private ArrayList<Card> discardPile;
+
+    // Method to start the game
+    public void playGame() {
+        // Implementation of the game logic goes here
+    }
+}
