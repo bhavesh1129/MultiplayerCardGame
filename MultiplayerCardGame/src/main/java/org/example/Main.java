9e0a45c73bc98d55d472ed5a3372964f226c64d0
Bhavesh Garg <developerbhaveshgarg@gmail.com>
2023-04-12 10:51:28 +0530

    Add methods in Main class

--- MultiplayerCardGame/src/main/java/org/example/Main.java
+++ MultiplayerCardGame/src/main/java/org/example/Main.java
@@ -1,5 +1,14 @@
+// Package name and imports
 package org.example;
 
+import org.example.entity.GamePlay;
+
 public class Main {
-    
+    public static void main(String[] args) throws Exception {
+
+        // Creating a GamePlay object and calling the playGame method
+        GamePlay game = new GamePlay();
+        game.playGame();
+    }
 }
+// end of Main class
